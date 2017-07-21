1d225473150f10a7be286c3895643fc572320805
Eliza Weisman <eliza@buoyant.io>
2017-07-21 13:22:59 -0500

    Start on making paths actually usable

--- src/path.rs
+++ src/path.rs
@@ -1,4 +1,4 @@
-use std::{convert, fmt, iter, ops};
+use std::{convert, fmt, iter, ops, str};
 
 pub struct Path<'bytes>(pub Vec<&'bytes [u8]>);
 
@@ -9,6 +9,18 @@ impl<'bytes> Path<'bytes> {
         self.0.push(path.into());
         Ok(self)
     }
+}
+
+impl<'a, 'b, R> ops::Div<&'b R> for Path<'a>
+where R: convert::AsRef<[u8]>
+    , 'b: 'a
+    {
+    type Output = Self;
+    fn div(mut self, rhs: &'b R) -> Self {
+        self.0.push(rhs.as_ref());
+        self
+    }
+
 }
 
 impl<'a, 'b, R> ops::Div<R> for &'a mut Path<'a>
@@ -36,6 +48,37 @@ where T: convert::Into<&'b [u8]>
     }
 }
 
+//impl<'a, 'b, I> convert::From<I> for Path<'a>
+//where I: convert::Into<&'b [u8]>
+//    , 'b: 'a
+//    {
+//    #[inline] fn from(i: I) -> Path<'a> {
+//        Path(vec![i.into()])
+//    }
+//}
+
+impl<'a, A> convert::From<&'a A> for Path<'a>
+where A: convert::AsRef<[u8]>
+    , A: 'a {
+    #[inline] fn from(bytes: &'a A) -> Path<'a> {
+        Path(vec![bytes.as_ref()])
+    }
+}
+
+
+impl <'a> fmt::Display for Path<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for ref elem in &self.0 {
+            write!(f, "/{}", unsafe {
+                // validity of ascii chars should already be checked
+                // when elements are added to a path...
+                str::from_utf8_unchecked(elem)
+            })?;
+        }
+        Ok(())
+    }
+}
+
 pub enum PathError<'bytes> {
     InvalidCharacter { ch: char, at: usize, elem: &'bytes str }
 }
@@ -50,5 +93,17 @@ impl<'bytes> fmt::Debug for PathError<'bytes> {
                     , at = at
                     , elem = elem )
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn path_slash_operator() {
+        let path = Path::from(b"aaaa") / b"bbbb";
+        assert_eq!(format!("{}", path), "/aaaa/bbbb")
+
     }
 }
